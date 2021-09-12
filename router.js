3d9bebe3bbcd48f2a34cede4eeae178d636670c7
Abdulrahman M <aoofxd@gmail.com>
2021-09-12 16:11:45 -0400

    Adding more information about me

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const router        = require("express").Router();
 const controller    = require("./controllers/main");
 
 router.get('/',                 controller.home);
-router.get('/admin',            controller.admin);
-router.post('/admin-login',     controller.login);
-router.post('/admin-logout',    controller.mustBeLoggedIn, controller.logout);
+// router.get('/admin',            controller.admin);
+// router.post('/admin-login',     controller.login);
+// router.post('/admin-logout',    controller.mustBeLoggedIn, controller.logout);
 
 module.exports = router;
