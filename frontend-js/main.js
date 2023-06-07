a1c38448bd829f5b21eefa9a205dcfa591fc7526
Aoof <53632818+Aoof@users.noreply.github.com>
2023-06-06 21:38:03 -0400

    Removed clock

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,10 +1,5 @@
 import VerySecretCompartment from "./modules/verysecretcompartment"
-import Clock from "./modules/clock"
 
 if (document.querySelector("#vsc")) {
     new VerySecretCompartment()
-}
-
-if (document.querySelector(".clock")) {
-    new Clock()
 }
